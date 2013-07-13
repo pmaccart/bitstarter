574125df6dc630283c70cebe12be49bc826f4720
pmaccart <pmaccart@alumni.nd.edu>
2013-07-13 09:27:02 -0500

    update web.js to send back contents of index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  //response.send('Hello World!');
+  var index = fs.readFileSync('index.html', 'utf8');
+  response.send(index);
 });
 
 var port = process.env.PORT || 5000;
